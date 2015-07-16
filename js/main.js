590591a9115e246514c1bd6db76b41d035800908
unknown <jpariona@wsjpariona.americatv.cpr.com.pe>
2015-07-16 15:36:50 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var swiper = new Swiper('.swiper-container',{
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+});
