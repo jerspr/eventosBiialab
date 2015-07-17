a21b4f9c35e91a60b5a7ead6ab9aff38872c5423
unknown <jpariona@wsjpariona.americatv.cpr.com.pe>
2015-07-17 15:25:47 -0500

    update

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
         spaceBetween: 30,
         loop: true
     });
+
+    $('.responsive-calendar').responsiveCalendar({
+    	events: {
+            "2015-07-16": {
+            	"number": 5,
+            	"badgeClass": "badge-evento",
+            	"url": "calendario.html"
+            },
+            "2015-07-10": {"number": 1, "badgeClass": "badge-evento", "url": "calendario.html"}
+        }
+    });
 });
