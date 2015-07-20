633f2429d9f3f9070211a868ff7e58f975a28380
unknown <jpariona@wsjpariona.americatv.cpr.com.pe>
2015-07-20 16:44:26 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	var swiper = new Swiper('.swiper-container',{
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+
+    $('.responsive-calendar').responsiveCalendar({
+    	events: {
+            "2015-07-16": {
+            	"number": 5,
+            	"badgeClass": "badge-evento",
+            	"url": "calendario.html"
+            },
+            "2015-07-10": {"number": 1, "badgeClass": "badge-evento", "url": "calendario.html"}
+        }
+    });
+});
